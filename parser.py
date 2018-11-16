f1f182e4283dcc4b8b8520cc54c230d4db6e8dde
@volflow <valentin@valentin-wolf.de>
2018-11-16 13:56:09 -0800

    scoring implemented

--- parser.py
+++ parser.py
@@ -1,7 +1,9 @@
 from lxml import html,etree
 import re
 import os
+import json
 
+bookeeping_fp = './WEBPAGES_RAW/bookkeeping.json'
 def parse(fn):
     tree = html.parse(fn)
 
@@ -17,41 +19,113 @@ def parse(fn):
 
     return ' '.join(text) # TODO: make it wokt without join
 
-def import_bookeeping(fn):
-    
+def import_id_to_url(json_fp=bookeeping_fp):
+    with open(json_fp) as f:
+        id_to_url = json.load(f)
+
+    return id_to_url
+
+def build_index():
+    id_to_url = import_id_to_url(bookeeping_fp)
+    index = Index(id_to_url=id_to_url)
+
+    print('Parsing Documents...')
+    for doc_id in id_to_url:
+        fp = './WEBPAGES_RAW/' + doc_id
+
+        # map
+        title,metadata,body = parse(fp)
+
+        token_dict = {}
+        for str_ in title:
+            tokens = tokenize(str_,3)
+            token_dict = insert_token_dict(tokens,type=0,dict_=token_dict)
+        for str_ in metadata:
+            tokens = tokenize(str_,3)
+            token_dict = insert_token_dict(tokens,type=1,dict_=token_dict)
+        for str_ in body:
+            tokens = tokenize(str_,3)
+            token_dict = insert_token_dict(tokens,type=2,dict_=token_dict)
+
+        # reduce
+        for token,token_freq in token_dict.iteritems():
+            index.add(doc_id,token,token_freq)
+
+        #print(doc_id,fp)
+
+    print('Calculating Scores...')
+    index.update_scores()
+
+    print('done')
+    return index
+
+
 
 def tokenize(s, min_len):
     # find all alpha numerical tokens of len  >= min_len
     s = re.findall('[a-zA-Z0-9]{%d}[a-zA-Z0-9]*' % min_len, s)
     return map(lambda x: x.lower(), s)  # make all tokens lowercase
 
-def insert_token_dict(tokens, dict_ = {}, count=True):
+def insert_token_dict(tokens, dict_ = {}, type, count=True):
+    """
+    type: 0 title | 1 metadata | 2 body
+    """
     for t in tokens:
         t.lower()
-        dict_.setdefault(t, 0)
+        dict_.setdefault(t, (0,0,0))
         if count:
-            dict_[t] += 1
+            dict_[t][type] += 1
     return dict_
 
 
 class Index(object):
-    def __init__(self, dict_={}):
-        self.dict_ = dict_
+    def __init__(self, token_to_id_metadata={}, id_to_url={}):
+        self.id_to_url = id_to_url
+        self.token_to_id_metadata = token_to_id_metadata
+        self.token_to_id_score = {}
 
     def add(self,doc_id,token,token_freq):
+        self.token_to_id_metadata.setdefault(token, [])
+        self.token_to_id_metadata[token].append((doc_id,token_freq))
 
-        self.dict_.setdefault(token, [])
-
-        self.dict_[token].append((doc_id,token_freq))
+    def update_scores(self):
+        self.token_to_id_score = {}
+        for token in self.token_to_id_metadata:
+            id_score = self.score(token)
+            self.token_to_id_score[token] = id_score
 
     def search(self,query):
         tokens = tokenize(query,min_len=3)
 
         results = []
         for token in tokens:
-            if token in self.dict_:
-                found = self.dict_[token]
-                found.sort(key=lambda x: x[1], reverse=True)
+            if token in self.token_to_id_score:
+                found = self.token_to_id_score[token]
+                #scored = self.score(token,found)
             results += found
+            break
 
+        def add_urls(id_tuple):
+            url = self.id_to_url[id_tuple[0]]
+            return url, id_tuple
+
+        results = map(add_urls,results)
         return results
+
+    def score(self,token):
+        postings_list = self.token_to_id_metadata[token]
+        idf = len(self.token_to_id_metadata[token])/len(self.id_to_url) # df/total_documents
+
+        def tf_idf(x):
+            score = x[1] * idf
+            return x[0], score#, x[1], idf#token,score
+
+        scored_postings = list(map(tf_idf,postings_list))
+        # sort scored_postings
+        scored_postings.sort(key=lambda x: x[1], reverse=True)
+
+        return scored_postings
+
+    # def rank(self,id_tuple_lsit):
+    #     'in-place ranking'
+    #     id_tuple.sort(key=lambda x: x[1], reverse=True)
